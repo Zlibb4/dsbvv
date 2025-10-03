69feb57d794e42522c3f3524bab4bffa02b09cba
Zlibb4 <zlibbrary4@gmail.com>
2025-10-03 15:38:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import random
 # =============================
 
 # Hugging Face model IDs
-SMOLLM_MODEL_ID = "JustToTryModels/self16"
+SMOLLM_MODEL_ID = "HuggingFaceTB/SmolLM2-135M-Instruct"
 CLASSIFIER_ID = "IamPradeep/Query_Classifier_DistilBERT"
 
 # Random OOD Fallback Responses
